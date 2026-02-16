a760e5c69da60d099d47812add5d6bc384ecf44e
Lorenzo Rodigliano <rodiglianolorenzo@gmail.com>
2026-02-16 17:54:53 +0100

    compelte the exercise

--- js/script.js
+++ js/script.js
@@ -6,36 +6,59 @@ const instructionsEl = document.getElementById("instructions")
 const numbersListEl = document.getElementById("numbers-list")
 //seleziono il form dal HTML 
 const answersFromEl = document.getElementById("answers-form")
-
+const formControlEl = document.querySelectorAll(".form-control")
+const messageEl = document.getElementById("message")
 
 let numbGen = [];
-let countdown = 10;
+let userNumb = [];
+let countdown = 3;
+
 
 numbersListEl.innerHTML = countdown--;
 
- for (let i = 0; i < 5; i++) {
-        let rundomNum = Math.floor(Math.random() * 4) + 1;
-        numbGen.push(rundomNum)
-        numbersListEl.innerText = numbGen
-    }
 
- numbGen = setInterval(function () {
-   
-    
+for (let i = 0; i < 5; i++) {
+    let rundomNum = Math.floor(Math.random() * 4) + 1;
+    numbGen.push(rundomNum)
+    numbersListEl.innerText = numbGen
+}
+
+
+let timer = setInterval(function () {
+    countdown--;
+
     if (countdown === 0) {
-        clearInterval(numbGen);
+        clearInterval(timer);
+
         answersFromEl.classList.remove('d-none')
         numbersListEl.remove()
     } else {
         countdownEl.innerHTML = countdown;
     }
-    countdown--;
 
-    
 
 
 }, 1000)
 
+answersFromEl.addEventListener("submit", function (e) {
+    e.preventDefault()
 
- 
+    userNumb = [];
+
+    formControlEl.forEach((input) => {
+        userNumb.push(parseInt(input.value))
+    });
+
+    let guessedNumbers = [];
+
+    userNumb.forEach((number) => {
+        if (numbGen.includes(number)) {
+            guessedNumbers.push(number);
+        }
+    });
+    messageEl.innerHTML = `Hai indovinato ${guessedNumbers.length} numeri : ${guessedNumbers.join(",")}`
+});
+
+
+
 
